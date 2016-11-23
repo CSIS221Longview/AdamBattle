4394ccc98b08ae1048cc9a3782badc2becca4dda
ABoehm4 <adamboehm04@gmail.com>
2016-11-23 15:56:16 -0600

    updated with char array

--- gameboard.java
+++ gameboard.java
@@ -3,25 +3,24 @@ package battleship;
 import java.security.SecureRandom;
 
 public class gameboard {
-	int BOARD_SIZE;
+    int BOARD_SIZE;
 	
-	int [][] board;
-	int [][] ships;
+	char [][] board;
+	char [][] ships;
 	
 
 public void setboard() {
 	// setting the array size depending on the game difficulty
-	board = new int[BOARD_SIZE][BOARD_SIZE];
-	ships = new int[BOARD_SIZE][BOARD_SIZE];
+	board = new char[BOARD_SIZE][BOARD_SIZE];
+	ships = new char[BOARD_SIZE][BOARD_SIZE];
 	fillboard();
 }
 	
 private void fillboard() {
 	// initializing both the board and ship array to -1
-	for(int row = 0; row < this.BOARD_SIZE; row++) {
-		for (int column = 0; column < this.BOARD_SIZE; column++) {
-			ships[row][column] = -1;
-			board[row][column] = -1;
+	for(int i = 0; i < this.BOARD_SIZE; i++) {
+		for (int j = 0; j < this.BOARD_SIZE; j++) {
+			board[i][j] = '~';
 		}
 	}
 }
@@ -30,30 +29,23 @@ private void fillboard() {
 
 public void printboard() {
 	// for printing the top column of the game board going to the right
+	System.out.printf("%nBoard size is: %dx%d%n", this.BOARD_SIZE, this.BOARD_SIZE);
 	System.out.print("\n   ");
-	for (int column = 0; column < this.BOARD_SIZE; column++) {
-		System.out.printf("%3d", column+1);	
+	for (int i = 0; i < this.BOARD_SIZE; i++) {
+		System.out.printf("%3d", i+1);	
 	}
 	// start of main loop for printing the rows for the rest of the board
 	// for printing the numbers (converted to letters) going down
-	for (int row = 0; row < this.BOARD_SIZE; row++) {
+	for (int i = 0; i < this.BOARD_SIZE; i++) {
 		System.out.print("\n");
-		System.out.printf("%3c", getLetter(row+1));
-		for (int column = 0; column < this.BOARD_SIZE; column++) {
-		    if (board[row][column] == -1){
-                System.out.print("  ~");
-            }
-		    else if(board[row][column] == 0){
-                System.out.print("  *");
-            }
-		    else if(board[row][column] == 1){
-                System.out.print("  X");
+		System.out.printf("%3c", getLetter(i+1));
+		for (int j = 0; j < this.BOARD_SIZE; j++) {
+			
             }
             
-        }
-		
-	}
+    }
 }
+
 
 // converting the initial column for printing to letters
 public char getLetter(int i){
