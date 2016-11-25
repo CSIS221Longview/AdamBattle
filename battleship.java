808acdc6b8bf50b43dcb12fccdeb3dee65f0cc52
ABoehm4 <adamboehm04@gmail.com>
2016-11-25 12:16:24 -0600

    Update battleship.java

--- battleship.java
+++ battleship.java
@@ -1,171 +1,88 @@
 package battleship;
 
-import java.util.Random;
+import java.util.Scanner;
 
-public class gameboard {
-    int BOARD_SIZE;
-    int missilesleft;
-    static int size;
-    private int shipsalive = 5;
-    private static final int AIRCRAFT_CARRIER = 5;
-    private static final int BATTLESHIP = 4;
-    private static final int DESTROYER = 3;
-    private static final int SUBMARINE = 3;
-    private static final int PATROL = 2;
-   
-	
-	int [][] board;
-	int [] ships = new int[5];
+public class battleship {
+	static gameboard board = new gameboard();
 	
 
-public void setboard() {
-	// setting the array size depending on the game difficulty
-	board = new int[BOARD_SIZE][BOARD_SIZE];	
-	ships[0] = AIRCRAFT_CARRIER;
-	ships[1] = BATTLESHIP;
-	ships[2] = DESTROYER;
-	ships[3] = SUBMARINE;
-	ships[4] = PATROL;
-	fillboard();
-}
-
-	
-private void fillboard() {
-	// initializing both the board and ship array to ~
-	for(int i = 0; i < this.BOARD_SIZE; i++) {
-		for (int j = 0; j < this.BOARD_SIZE; j++) {
-			board[i][j] = -1;
-		}
-	}
-	setShips();
-}
-
-
-
-public void printboard() {
-	 char water = '~';
-	 char miss = '*';
-	 char hit = 'X';
-	 char air_carrier = 'A';
-	 char battleship = 'B';
-	 char destroyer = 'D';
-	 char sub = 'S';
-	 char patrol = 'P';
-	// printout for each turn indicating missiles left, ships still floating
-	System.out.println("--------------------------------------------");
-	System.out.printf("Board size is: %dx%d%n", this.BOARD_SIZE, this.BOARD_SIZE);
-	System.out.println("Missiles Left: " + this.missilesleft);
-	System.out.println("Ships Floating: " + this.shipsalive);
-	
-	// for printing the top column of the game board going to the right with some extra space
-	System.out.print("\n  ");
-	for (int i = 0; i < this.BOARD_SIZE; i++) {
-		System.out.printf("%3d", i+1);	
-	}
-	// start of main loop for printing the rows for the rest of the board
-	// for printing the numbers (converted to letters) going down
-	for (int i = 0; i < this.BOARD_SIZE; i++) {
-		System.out.print("\n");
-		System.out.printf("%3c", getLetter(i+1));
-		for (int j = 0; j < this.BOARD_SIZE; j++) {
-			if (board[i][j] == -1)
-				System.out.printf("%2c", water);
-			else if (board[i][j] == 0) 
-				System.out.printf("%2c", miss);
-			else if (board[i][j] == 1)
-				System.out.printf("%2c", hit);
-			// the rest are just for testing purposes only, will remove once the program is done
-			else if (board[i][j] == -3)
-				System.out.printf("%2c", air_carrier);
-			else if (board[i][j] == -4)
-				System.out.printf("%2c", battleship);
-			else if (board[i][j] == -5)
-				System.out.printf("%2c", destroyer);
-			else if (board[i][j] == -6)
-				System.out.printf("%2c", sub);
-			else if (board[i][j] == -7)
-				System.out.printf("%2c", patrol);
-			System.out.print("|");
-            }
-            
-    }
-}
-
-public void setShips() {
-	for (int ship = 0; ship < ships.length; ship++) {
-		Random rand = new Random();
-		int direction = rand.nextInt(2)+1;
-		int coordX;
-		int coordY;
-		coordX = rand.nextInt(this.BOARD_SIZE);
-		coordY= rand.nextInt(this.BOARD_SIZE);
-		int shiplength = ships[ship];
-		int shipChar = ship;
-	
-		if (board[coordX][coordY] != -1)
-			continue;
-		if (shiplength + coordX > this.BOARD_SIZE - 1)
-			continue;
-			// Horizontal placement if direction = 1
-		if (direction == 1) {
-			for (int i = 0; i < shiplength; i++) {
-				// if else statement for setting the value of the ship for printing
-				// -3 = aircraft carrier
-				// -4 = battleship
-				// -5 = destroyer
-				// -6 = submarine
-				// -7 = patrol boat
-				if (shipChar == 0) 
-					board[coordX + i][coordY] = -3;
-				else if (shipChar == 1)
-					board[coordX + i][coordY] = -4;
-				else if (shipChar == 2)
-					board[coordX + i][coordY] = -5;
-				else if (shipChar == 3)
-					board[coordX + i][coordY] = -6;
-				else if (shipChar == 4)
-					board[coordX + i][coordY] = -7;
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int gamediff = 0;
+		
+		 		
+		gameLogo();
+		gameRules();
 				
-			}
+		System.out.print("Select your board size by entering the number 1, 2, or 3:  ");
+		gamediff = input.nextInt();
+		
+		while (gamediff != 1 && gamediff != 2 && gamediff != 3) {
+			System.out.println("\nYou have entered an invalid difficulty entry\n");
+			System.out.print("Select your board size by entering the number 1, 2, or 3:  ");
+			gamediff = input.nextInt();			
 		}
-			
-		if (shiplength + coordY > this.BOARD_SIZE - 1)
-			continue;
-			// Vertical placement if direction = 2
-		else if (direction == 2) {
-			for (int i = 0; i < shiplength; i++) {
-				// if else statement for setting the value of the ship for printing
-				// -3 = aircraft carrier
-				// -4 = battleship
-				// -5 = destroyer
-				// -6 = submarine
-				// -7 = patrol boat
-				if (shipChar == 0) 
-					board[coordX][coordY + i] = -3;
-				else if (shipChar == 1)
-					board[coordX][coordY + i] = -4;
-				else if (shipChar == 2)
-					board[coordX][coordY + i] = -5;
-				else if (shipChar == 3)
-					board[coordX][coordY + i] = -6;
-				else if (shipChar == 4)
-					board[coordX][coordY + i] = -7;
-				}
-				}	
-			}
+		switch (gamediff) {
+		case 1: System.out.println("\nYou have chosen beginner. You must be scared.");
+				board.BOARD_SIZE=6;
+				board.missilesleft=30;
+				break;
+		case 2: System.out.println("\nYou have chosen Standard. Just average eh?");
+				board.BOARD_SIZE=9;
+				board.missilesleft=50;
+				break;
+		case 3: System.out.println("\nYou have chosen Advanced. You must be confident!");
+				board.BOARD_SIZE=12;
+				board.missilesleft=75;
+		 		break;	
+		}
+		
+		board.setboard();
+		board.printboard();
+	
+	
+		input.close();
+
 		
 	}
 
+	
 
-// converting the initial column for printing to letters
-public char getLetter(int i){
-  return (char) (i + 64);
-}
+
+	private static void gameLogo() {
+	System.out.println("-----------------------------------------------------------------------------");
+	System.out.println("| @@@@@@@@                                     @@@@@@@  @                   |");
+	System.out.println("| @@    @@                                    @         @                   |");
+	System.out.println("| @@    @@               @      @   @        @          @       @           |");
+	System.out.println("| @@    @@               @      @   @       @           @                   |");
+	System.out.println("| @@@@@@@@               @      @   @        @          @                   |");
+	System.out.println("| @@@@@@@@@              @      @   @          @        @                   |");
+	System.out.println("| @@     @@  @@@@@@@@  @@@@@  @@@@@ @ @@@@@     @@@     @@@@@@  @  @@@@@@@  |");
+	System.out.println("| @@     @@  @      @    @      @   @ @             @   @    @  @  @     @  |");
+	System.out.println("| @@     @@  @      @    @      @   @ @@@@           @  @    @  @  @     @  |");
+	System.out.println("| @@     @@  @      @    @      @   @ @               @ @    @  @  @     @  |");
+	System.out.println("| @@     @@  @      @    @      @   @ @              @  @    @  @  @     @  |");
+	System.out.println("| @@@@@@@@@  @@@@@@@@@   @      @   @ @@@@@    @@@@@@   @    @  @  @@@@@@@  |");
+	System.out.println("|                                                                  @        |");
+	System.out.println("|                                                                  @        |");
+	System.out.println("|                                                                  @        |");
+	System.out.println("|                                                                  @        |");
+	System.out.println("-----------------------------------------------------------------------------");	
+	}
+	
+	private static void gameRules() {
+	System.out.println("You have three difficulties to choose from");
+	System.out.println("   LEVEL        BOARD SIZE        MISSLES");
+	System.out.println("1. Beginner        6x6               30   ");
+	System.out.println("2. Standard        9x9               50   ");
+	System.out.println("3. Advanced       12x12              75   ");
+		
+	}
+	
 
 
 	
 }
-
 
 
 
