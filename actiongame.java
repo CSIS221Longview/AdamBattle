d1a8d53a6300ff7c4bed51ad1f922a375153b946
ABoehm4 <adamboehm04@gmail.com>
2016-11-30 22:20:11 -0600

    Slowly finalizing the program

--- actiongame.java
+++ actiongame.java
@@ -3,63 +3,119 @@ import java.util.Scanner;
 
 public class actiongame {
 
-	gameboard gameboard = new gameboard();
+	gameboard gameboard = new gameboard(); // create new object of gameboard.java
 	Scanner kb = new Scanner(System.in);
 	
 	public void startGame() {
-		gameboard.setDiff();
-		gameboard.placeShips();
-		gameboard.printBoard();
+		gameboard.setDiff(); // call the setDiff() method from gameboard.java
+		gameboard.placeShips(); // call placeShips() method from gameboard.java
+		gameboard.printBoard(); // call the printBoard() method from gameboard.java
 	}
 	
 	public void playGame() {
-		boolean gameOver;
+		boolean gameOver; // create a boolean for determing gameover
+		
+			// the following code will execute over and over until gameOver is set to true (when the game ends)
 		do {
-		gameOver = false;
-		System.out.print("\n\nWhat row do you want to attack? (A-F): ");
-		char input = kb.next().charAt(0); // takes character input starting at position 0
-		input = Character.toUpperCase(input); // capatalizes the character inputted for easier conversion to integer
-		// test for uppercase System.out.printf("%nYou have entered %c as your char input", input);
+			// initializing gameOver to false
+		gameOver = false; 
+		System.out.print("\n\nWhat row do you want to attack? (example: A or b): ");
+		
+		char input = kb.next().charAt(0); // takes the character input from the player 
+		input = Character.toUpperCase(input); // capatalizes the character input for easier conversion to integer
+		// this is merely a test for uppercase System.out.printf("%nYou have entered %c as your char input", input);
+		
+		
 		int row = getNumber(input); // passes the character input to getNumber() to convert the letter to a number for the board[][] array
-		System.out.print("What column do you want to attack? (1-6): ");
+		System.out.print("What column do you want to attack? (example: 1 or 5): ");
 		int col = kb.nextInt() - 1; // subtract 1 so we don't go out of bounds on the board[][] array
-		fire(row, col); // pass the converted row and column to fire method to attack the board
-	
-		} while(!gameOver);
+		
+		// pass the converted row and column to fire method to attack the board
+		fire(row, col); 
+		gameboard.useMissiles();
+		// call printBoard() from the gameboard.java to re-print the board for another attack
+		gameboard.printBoard();
+		// once the int sunk value hits 0, all ships have been sunk, the player wins, call gameOverWin() from gameboard.java
+		if (gameboard.shipsAlive == 0) {
+			gameboard.gameOverWin();
+			gameOver = true;
+		}
+		// or if they have used all their missiles, the player loses, call gameOverLoss() from gameboard.java
+		else if (gameboard.getMissiles() == 0) {
+			gameboard.gameOverLoss();
+			gameOver = true;
+		}
+		} while(!gameOver); // the do, while loop will end when gameOver is true
 	}
 	
+	
+	// The following code will determine what the player hit with their choice of attack
+	// the row and col values were passed inside the playGame() method
 	public void fire(int row, int col) {
+		// if the players attack landed on a -3 value, they hit the AIRCRAFT_CARRIER
 		if (gameboard.board[row][col] == -3){
+			// Create an int for ship sinking call sunk. Each time the players attack lands on a -3 value, subtract 1 from the ships size
+			// once the size hits 1, the ship is sunk
 			int sunk = gameboard.ships[0].size--;
-			System.out.printf("You have hit the %s", gameboard.ships[0].name);
+			System.out.printf("%nYou have hit the %s", gameboard.ships[0].name);
+			// change the value where the player attacked to a 1, meaning a HIT
 			gameboard.board[row][col] = 1;
 			if (sunk == 1) {
-				System.out.printf("%n You have sunk the %s", gameboard.ships[0].name);
+				System.out.printf("%n You have sunk the %s Aircraft Carrier.", gameboard.ships[0].name);
 				gameboard.shipsAlive--;
 			}
 		}
+		// if the players attack landed on a -4 value, they hit the BATTLESHIP
 		else if (gameboard.board[row][col] == -4) {
-			System.out.printf("You have hit the %s", gameboard.ships[1].name);
+			int sunk = gameboard.ships[1].size--;
+			System.out.printf("%nYou have hit the %s", gameboard.ships[1].name);
 			gameboard.board[row][col] = 1;
+			if (sunk == 1) {
+				System.out.printf("%n You have sunk the %s Battleship.", gameboard.ships[1].name);
+				gameboard.shipsAlive--;
+			}
 		}
+		// if the players attack landed on a -5 value, they hit the DESTROYER
 		else if (gameboard.board[row][col] == -5) {
-			System.out.printf("You have hit the %s", gameboard.ships[2].name);
+			int sunk = gameboard.ships[2].size--;
+			System.out.printf("%nYou have hit the %s", gameboard.ships[2].name);
 			gameboard.board[row][col] = 1;
+			if (sunk == 1) {
+				System.out.printf("%nYou have sunk the %s Destroyer.", gameboard.ships[2].name);
+				gameboard.shipsAlive--;
+			}
 		}
+		// if the players attack landed on a -6 value, they hit the SUBMARINE
 		else if (gameboard.board[row][col] == -6) {
-			System.out.printf("You have hit the %s", gameboard.ships[3].name);
+			int sunk = gameboard.ships[3].size--;
+			System.out.printf("%nYou have hit the %s", gameboard.ships[3].name);
 			gameboard.board[row][col] = 1;
+			if (sunk == 1) {
+				System.out.printf("%n You have sunk the %s Submarine.", gameboard.ships[3].name);
+				gameboard.shipsAlive--;
+			}
 		}
+		// if the players attack landed on a -7 value, they hit the PATROL_BOAT
 		else if (gameboard.board[row][col] == -7) {
-			System.out.printf("You have hit the %s", gameboard.ships[4].name);
+			int sunk = gameboard.ships[4].size--;
+			System.out.printf("%nYou have hit the %s", gameboard.ships[4].name);
 			gameboard.board[row][col] = 1;
+			if (sunk == 1) {
+				System.out.printf("%n You have sunk the %s Patrol Boat.", gameboard.ships[4].name);
+				gameboard.shipsAlive--;
+			}
 		}
+		
+		// if the players attack hit anything other than the values indicated above, then they missed hitting a ship
 		else {
 			System.out.print("You hit NOTHING! TRY AGAIN");
+			// assign the value of the board where the player attacked to 0, meaning a miss
 			gameboard.board[row][col] = 0;
 		}
-		gameboard.useMissiles();		
-		gameboard.printBoard();
+		
+		
+		
+	
 	}
 	
 	
