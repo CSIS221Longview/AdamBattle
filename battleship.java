f33144c4a9d81c89d943fb4b23be85245385d25f
ABoehm4 <adamboehm04@gmail.com>
2016-12-02 09:33:50 -0600

    Update battleship.java

--- battleship.java
+++ battleship.java
@@ -1,12 +1,11 @@
 package battleship;
 
 
-public class battleship {
+public class battleship extends actiongame {
 	public static void main(String[] args) {
 		// creating a new object of actiongame.java
 	actiongame action = new actiongame(); 
 	
-	gameboard.getLogo(); // get the gameboard logo
 	action.startGame(); // call actiongame.java and start the game
 	action.playGame(); // call actiongame.java and play the game
 	}
